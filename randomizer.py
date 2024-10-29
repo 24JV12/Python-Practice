1aa6c863db9cd882d501c50164cb275263e3484e
24JV12 <159084205+24JV12@users.noreply.github.com>
2024-10-29 22:00:21 +0530

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -1,32 +1,27 @@
-from random import choice
-import os
+import os, random, time
+def clear(): os.system('cls' if os.name == 'nt' else 'clear')
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def pick_one():
-    jar = []
-    interval = 1
-    while True:
-        cmd = input(f"Choice {interval}: ")
-        command = cmd.lower()
-        if command == "done":
-            break
-        elif command == "clear" or command == "cls":
-            clear()
-        elif command == "exit":
-            exit()
-        else: 
-            jar.append(cmd)
-            interval += 1
-            continue
-
-    if jar:
-        print(f"Random Pick is '{choice(jar)}'")
-    else:
-        print("No choices were entered.")
+def randomize(jar, interval, cls):
+    clear()
+    removals = len(jar) - 1
+    for _ in range(removals):
+        unfaithful = random.choice(jar)
+        jar.remove(unfaithful)
+        if not cls: print(f"'{unfaithful}' is out of the game!"), time.sleep(interval)
+    if cls: clear()
+    if jar: print(f"And the random choice is '{jar[0]}'")
 
 clear()
-
 while True:
-    pick_one()
+    jar = []
+    while True:
+        cmd = input(f"Choice {len(jar) + 1}: ").strip()
+        if cmd.lower().startswith("done"):
+            parts = cmd.split()
+            if len(parts) == 2 and parts[-1].isdigit(): interval, cls = int(parts[1]), False
+            else: interval, cls = 0, True
+            randomize(jar, interval, cls)
+            break
+        elif cmd in ["cls", "clean", "clear"]: clear()
+        elif cmd == "exit": exit()
+        else: jar.append(cmd)

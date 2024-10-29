31e15edda54941d5a221422b70c4a4a8821a8014
24JV12 <159084205+24JV12@users.noreply.github.com>
2024-10-29 22:09:21 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,7 @@
-import time
-
+import time, random
 fibonacci = [0,1]
 print(f"{fibonacci[0]}\n{fibonacci[1]}")
 
 while True:
     fibonacci.append(fibonacci[-1] + fibonacci[-2])
-    time.sleep(0.1)
-    print(number)
+    time.sleep(random.uniform(0.01, 1.0)), print(number)

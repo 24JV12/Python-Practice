6ad3ac4511b8e3127b9dfd094cd7088bdebd2df1
24JV12 <159084205+24JV12@users.noreply.github.com>
2024-10-31 18:06:25 +0530

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,14 +1,17 @@
 from random import randint
 
-number = randint(int(input("Initial: ")), int(input("Final: ")))
+try: number = randint(int(input("Initial: ")), int(input("Final: ")))
+except ValueError: print("Error!"); exit()
 
 while True:
-    total_guesses = int(input("How many chances? [1, 25]: "))
-    if 1 < total_guesses < 25: break
-    print("Error Detected!")
+    try: 
+        total_guesses = int(input("How many chances? [1, 25]: "))
+        if 1 <= total_guesses <= 25: break
+    except ValueError: print("Error!")
 
 for guesses in range(1, total_guesses + 1):
-    guess = int(input(f"Guess {guesses}: "))
+    try: guess = int(input(f"Guess {guesses}: "))
+    except: print("Error!"); exit()
     if guess == 0: print(f"Number: {number}"); break
     elif guess == number: print("You Won!"); break
     elif guess > number: print("Try something less than that")

128ab2c57edf20f08f467badfdb88e8d626a3e52
24JV12 <159084205+24JV12@users.noreply.github.com>
2024-10-31 13:01:05 +0530

    Update ai_num_guess.py

--- ai_num_guess.py
+++ ai_num_guess.py
@@ -2,37 +2,24 @@ from random import randint
 import time, os
 
 def clear(): os.system('cls' if os.name == 'nt' else 'clear')
-clear()
+
 while True:
-    initial = randint(-100, 100)
-    number = randint(initial, randint(initial, 100))
-    while True:
-        total_guesses = int(input(r".\totalGuesses> "))
-        if total_guesses == 0: print("Don't be ridiculous")
-        else: break
-    print(f"Final Answer: {number}\n")
+    clear()
+    try: number, total_guesses = randint(-100, 100), int(input(r".\totalGuesses> ") or 5)
+    except ValueError: 
+        print("Non Integer Value Detected!")
+        break
+    low, high = -500, 500
 
-    low, high = -100, 100
-
-    for guesses in range(1, total_guesses + 1):
+    for guess_num in range(1, total_guesses + 1):
         guess = randint(low, high)
-        print(f"Guess {guesses}: {guess}")
+        print(f"Guess {guess_num}: {guess} {'Won!' if guess == number else ''}")
         
-        if guess == number:
-            print("Won!")
-            break
-        elif guess > number:
-            print("less than that\n")
-            high = guess - 1
-        else:
-            print("more than that\n")
-            low = guess + 1
+        if guess == number: break
+        elif guess > number: high = guess - 1; print("less than that\n")
+        else: low = guess + 1; print("more than that\n")
         
         time.sleep(1)
     else: print("Lost!")
-    while True:
-        againQ = input("Again? (Y/n) ").lower()
-        if "y" in againQ:
-            clear()
-            break
-        elif "n" in againQ: exit()
+    
+    if input("Again? (Y/n) ").lower() == 'n': break

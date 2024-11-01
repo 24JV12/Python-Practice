79987e75bffbd0459693cc84be5591f10a021f4a
24JV12 <159084205+24JV12@users.noreply.github.com>
2024-11-02 01:22:27 +0530

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -2,16 +2,20 @@ import os, random, time
 
 def clear(): os.system('cls' if os.name == 'nt' else 'clear')
 
-def randomize(jar, interval, cls):
+def randomize(jar, parts, interval, cls, remaining_choices):
     if not jar: print("No choices to pick from!"); return
-    clear(); removals = len(jar) - 1
-    for _ in range(removals):
-        unfaithful = random.choice(jar); jar.remove(unfaithful)
-        if not cls: print(f"'{unfaithful}' is out of the game!"),time.sleep(interval)
+    removals, executed = len(jar) - remaining_choices, False; clear()
+    if removals > 0 and remaining_choices != 0:
+        for _ in range(removals):
+            unfaithful = random.choice(jar); jar.remove(unfaithful)
+            if not cls: print(f"'{unfaithful}' is out of the game!"); time.sleep(interval)
+        executed = True
     if cls: clear()
-    if jar: print(f"And the random choice is '{jar[0]}'")
+    if jar and executed: print(f"Final choices are: {', '.join(jar)}" if len(parts) == 3 else f"Final choice is: {jar[0]}")
+    if not executed: print("Error!")
 
-def coin_flip(): time.sleep(1.5), print(random.choice(["Heads", "Tails"]))
+def coin_flip(): time.sleep(1.5); print(random.choice(["Heads", "Tails"]))
+
 clear()
 while True:
     jar = []
@@ -19,8 +23,8 @@ while True:
         cmd = input(f"Choice {len(jar) + 1}: ").strip()
         if cmd.lower().startswith("done"):
             parts = cmd.split()
-            interval, cls = int(parts[1]) if len(parts) == 2 and parts[-1].isdigit() else 0, len(parts) == 1
-            randomize(jar, interval, cls); break
+            randomize(jar, parts, float(parts[1]) if len(parts) > 1 and parts[1].isdigit() else 0,
+            len(parts) == 1, int(parts[2]) if len(parts) > 2 and parts[2].isdigit() else 1); break
         elif cmd.lower() in ["coin", "flip", "toss"]: coin_flip()
         elif cmd.lower() in ["cls", "clean", "clear"]: clear()
         elif cmd.lower() == "exit": exit()
